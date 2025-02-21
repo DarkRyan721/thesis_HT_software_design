dd7f089b31c97a114bf452e6306b117b9365852d
AlfredCue <alfredcuellv@gmail.com>
2025-02-21 17:45:29 -0500

    Ya se calcula el campo electroestatico y añadi los archivos de salida

--- src/Laplace_E_Solution.py
+++ src/Laplace_E_Solution.py
@@ -8,6 +8,7 @@ from ufl import ds, dx, grad, inner
 from dolfinx import fem
 from petsc4py import PETSc
 from dolfinx.fem import FunctionSpace
+import basix
 
 with io.XDMFFile(MPI.COMM_WORLD, "SimulationZone.xdmf", "r") as xdmf:
     domain = xdmf.read_mesh(name="SPT100_Simulation_Zone")  
@@ -33,54 +34,46 @@ V = fem.functionspace(domain, ("CG", 1))
 u_bc = fem.Function(V)
 u_bc.x.array[:] = np.nan
 
-for tag, value in [(3, 1.0), (4, 0.0)]:
-    dofs = fem.locate_dofs_topological(V, domain.topology.dim - 1, facet_tags.indices[facet_tags.values == tag])
-    u_bc.x.array[dofs] = value
+Volt = 10000
 
 boundary_conditions = []
-for tag in [3, 4]:
-    dofs = fem.locate_dofs_topological(V, domain.topology.dim - 1, facet_tags.indices[facet_tags.values == tag])
+for tag, value in [(3, Volt), (4, 0.0)]:  # Potencial fijo en facetas 3 y 4
+    dofs = fem.locate_dofs_topological(V, domain.topology.dim - 1,facet_tags.indices[facet_tags.values == tag])
+    u_bc.x.array[dofs] = value
     bc = fem.dirichletbc(u_bc, dofs)
     boundary_conditions.append(bc)
-
 
 with io.XDMFFile(MPI.COMM_WORLD, "boundary_conditions.xdmf", "w") as xdmf:
     xdmf.write_mesh(domain) 
     xdmf.write_function(u_bc)  
 
-print("Archivo 'boundary_conditions.xdmf' generado correctamente.")
-
-
-u = ufl.TrialFunction(V)
+phi = ufl.TrialFunction(V)
 v = ufl.TestFunction(V)
-x = ufl.SpatialCoordinate(domain)
-f = 10 * ufl.exp(-((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / 0.02)
-g = ufl.sin(5 * x[0])
-a = inner(grad(u), grad(v)) * dx
-L = inner(f, v) * dx + inner(g, v) * ds
-problem = LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
-uh = problem.solve()
+a = inner(grad(phi), grad(v)) * dx
+L = fem.Constant(domain, PETSc.ScalarType(0)) * v * dx #inner(f, v) * dx + inner(g, v) * ds
+problem = LinearProblem(a, L, bcs=boundary_conditions, petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+phi_h = problem.solve()
 
-with io.XDMFFile(domain.comm, "out_poisson/poisson.xdmf", "w") as file:
+
+with io.XDMFFile(domain.comm, "Laplace.xdmf", "w") as file:
     file.write_mesh(domain)
-    file.write_function(uh)
+    file.write_function(phi_h)
 
-try:
-    import pyvista
+#u_sol = fem.Function(V)
+#u_sol.interpolate(phi_h)
+#E_field = -grad(u_sol)
 
-    cells, types, x = plot.vtk_mesh(V)
-    grid = pyvista.UnstructuredGrid(cells, types, x)
-    grid.point_data["u"] = uh.x.array.real
-    grid.set_active_scalars("u")
-    plotter = pyvista.Plotter()
-    plotter.add_mesh(grid, show_edges=True)
-    warped = grid.warp_by_scalar()
-    plotter.add_mesh(warped)
-    if pyvista.OFF_SCREEN:
-        pyvista.start_xvfb(wait=0.1)
-        plotter.screenshot("uh_poisson.png")
-    else:
-        plotter.show()
-except ModuleNotFoundError:
-    print("'pyvista' is required to visualise the solution")
-    print("Install 'pyvista' with pip: 'python3 -m pip install pyvista'")
+
+V_vector = fem.functionspace(domain, ("CG", 1, (domain.geometry.dim,)))
+
+E_field = fem.Function(V_vector)
+
+E_field_expr = fem.Expression(-grad(phi_h), V_vector.element.interpolation_points())
+
+E_field.interpolate(E_field_expr)
+
+print(type(E_field))
+
+with io.XDMFFile(domain.comm, "Electric_Field.xdmf", "w") as xdmf:
+    xdmf.write_mesh(domain)
+    xdmf.write_function(E_field)
